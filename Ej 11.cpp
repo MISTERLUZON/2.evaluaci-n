33c201a8aa0b28e1a26c041cdafd400eb030dcfc
MISTERLUZON <55531071+MISTERLUZON@users.noreply.github.com>
2020-02-14 06:16:19 +0100

    Update Ej 11.cpp

--- Ej 11.cpp
+++ Ej 11.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <stdlib.h>
 
+//Lector caracteres
+
+using namespace std;
+
+int main(){
+	string palabra1,palabra2;
+	cout<<"Introduzca su primera palabra:   "; cin>>palabra1; cout<<"\nIntroduzca su segunda palabra:   "; cin>>palabra2;
+	if(palabra1[0]==palabra2[0]){cout<<"\nLa primera letra de cada palabra son iguales y esta es la "<<palabra1[0];}
+	else{cout<<"\nLa primera letra de las palabras son distintas ya que una es "<<palabra1[0]<<" y la otra es "<<palabra2[0];}
+	cout<<"\n\n";
+	system ("pause");
+	return 0;
+}
