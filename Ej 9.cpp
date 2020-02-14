ba4f42fadfd5a324157f8d11272b6db71b5d1f4a
MISTERLUZON <55531071+MISTERLUZON@users.noreply.github.com>
2020-02-14 07:17:00 +0100

    Update Ej 9.cpp

--- Ej 9.cpp
+++ Ej 9.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <stdlib.h>
 
+//hora
+
 using namespace std;
 
 int main(){
 	int horas; float coste;
-	cout<<"Introduzca las horas que ha trabajado:   "; cin>>horas; cout<<"\nIntroduzca el coste por horas:   "; cin>>coste; cout<<"\n\n";
+	cout<<"Introduzca las horas que ha trabajado:   "; 
+	cin>>horas;
+	cout<<"\nIntroduzca el coste por horas:   "; 
+	cin>>coste; 
+	cout<<"\n\n";
 	if(horas<=40){cout<<"El trabajador cobrara "<<horas*coste<<" euros por "<<horas<<" trabajadas\n";}
 	else{cout<<"El trabajador cobrara "<<(horas-40)*(1.5*coste)+40*coste<<" euros por "<<horas<<" trabajadas\n";}
 	cout<<"\n\n";
