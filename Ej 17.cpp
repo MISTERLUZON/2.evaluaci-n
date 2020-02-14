a9f502a5c75280070a5f5f2e727e8c354ac5554c
MISTERLUZON <55531071+MISTERLUZON@users.noreply.github.com>
2020-02-14 06:24:07 +0100

    Update Ej 17.cpp

--- Ej 17.cpp
+++ Ej 17.cpp
@@ -9,7 +9,9 @@ int main(){
 	const char *billetes[] = {"b500","b200","b100","b50","b20","b10","b5","m2","m1"};
 	const char *numero[] = 	{"500","200","100","50","20","10","5","2","1"};
 	int dinero,resto,gasto,coste;
-	cout<<"Introduzca la cantidad de dinero:   "; cin>>dinero; cout<<"\n\n"; int dinero2=dinero;
+	cout<<"Introduzca la cantidad de dinero:   ";
+	cin>>dinero; cout<<"\n\n";
+	int dinero2=dinero;
 	cout<<"Tienes: ";
 	for(int i=0; i<9; i++){
 		string x=numero[i];
@@ -20,7 +22,10 @@ int main(){
 			dinero=resto;
 		} 
 	}
-	cout<<"\n\nIntroduzca la cantidad de dinero a gastar:   "; cin>>coste; cout<<"\n\nTe devolveran: ";gasto = dinero2-coste;
+	cout<<"\n\nIntroduzca la cantidad de dinero a gastar:   "; 
+	cin>>coste; 
+	cout<<"\n\nTe devolveran: ";
+	gasto = dinero2-coste;
 	for(int i=0; i<9; i++){
 		string x=numero[i];
 		if(gasto/atoi(x.c_str())>=0){
