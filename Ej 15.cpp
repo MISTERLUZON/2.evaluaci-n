fd6056c95e7a8b25e1c60b1a531a7abbaea1f1ee
MISTERLUZON <55531071+MISTERLUZON@users.noreply.github.com>
2020-02-14 06:21:37 +0100

    Update Ej 15.cpp

--- Ej 15.cpp
+++ Ej 15.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <stdlib.h>
+#include <conio.h>
 
+//abecedario
+
+using namespace std;
+
+int main(){
+	string palabra;
+	cout<<"Introduzca su palabra:   "; cin>>palabra; cout<<"\n";
+	for(char i='a'; i<'z'; i++){
+		if(char(palabra[0])==i){cout<<"La primera letra de la palabra es la "<<i;}
+	}
+	cout<<"\n\n";
+	system("pause");
+	return 0;
+}
