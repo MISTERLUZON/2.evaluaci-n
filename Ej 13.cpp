42ed0db1a0f6082d4e71a92418805505387f1219
MISTERLUZON <55531071+MISTERLUZON@users.noreply.github.com>
2020-02-14 06:18:49 +0100

    Update Ej 13.cpp

--- Ej 13.cpp
+++ Ej 13.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <stdlib.h>
 
+//suma numero
+
+using namespace std;
+
+int main(){
+	string palabra; int suma=0, numero;
+	cout<<"Introduzca su numero:   "; cin>>palabra; cout<<"\n";
+	for(int i=0; i<=palabra.length(); i++){
+		numero = palabra[i];
+		suma = suma+numero;
+	}
+	cout<<"El resultado de la suma es "<<suma;
+	cout<<"\n\n";	
+	system("pause");
+	return 0;
