01829b8c2d52373c94a9d0d64f41e2049e33ba26
MISTERLUZON <55531071+MISTERLUZON@users.noreply.github.com>
2020-02-14 06:08:59 +0100

    Update Ej 5.cpp

--- Ej 5.cpp
+++ Ej 5.cpp
@@ -1,58 +1,29 @@
-#include<iostream>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
 
 int main(){
-    char salir;
-    int dia;
-    int mes;
-    int anno;
-    
-    std::cout<<"Bienvenido al programa para obtener una fecha.\n";
-    
-    std::cout<<"Introduzca su numero de dia: ";
-    std::cin>>dia;
-    
-    while (dia<=0){
-          std::cout<<"Su numero de dia no es valido. Es negativo o 0.\n";
-          std::cout<<"Introduzca aqui su numero de dia nuevamente: ";
-          std::cin>>dia;
-    }
-    
-    while (dia>31){
-          std::cout<<"Su numero de dia no es valido. Es superior a 31.\n";
-          std::cout<<"Introduzca de nuevo su numero de dia aqui: ";
-          std::cin>>dia;      
-    }
-    
-    std::cout<<"Introduzca su numero de mes: ";
-    std::cin>>mes;
-    
-    while (mes<=0){
-          std::cout<<"Su numero de mes no es valido. Es negativo o 0.\n";
-          std::cout<<"Introduzca aqui su numero de mes nuevamente: ";
-          std::cin>>mes;     
-    }
-    
-    while (mes>12){
-          std::cout<<"Su numero de mes no es valido. Es superior a 12.\n";
-          std::cout<<"Introduzca de nuevo su numero de mes aqui: ";
-          std::cin>>mes;
-    }
-    
-    std::cout<<"Introduzca su numero de anno: ";
-    std::cin>>anno;
-    
-    while (anno<0){
-          std::cout<<"Su numero de anno no es valido. Es negativo.\n";
-          std::cout<<"Introduzca aqui su numero de anno nuevamente: ";
-          std::cin>>anno;
-          }
-    
-    std::cout<<"Su fecha es: ";
-    std::cout<<dia;
-    std::cout<<" de ";
-    std::cout<<mes;
-    std::cout<<" de ";
-    std::cout<<anno;
-    
-    std::cin>>salir;
+	int dia,mes,years; char mess;
+	cout<<"Introduzca su fecha:\nDia: "; 
+    cin>>dia; 
+    cout<<"mes: ";
+    cin>>mes; cout<<"año: "; 
+    cin>>years;
+	if(mes==1){mess="Enero";} 
+    lse if(mes==2){mess="Febrero";} 
+    else if(mes==3){mess="Marzo";}
+	else if(mes==4){mess="Abril";} 
+    else if(mes==5){mess="Mayo";} 
+    else if(mes==6){mess="Junio";} 
+	else if(mes==7){mess="Julio";}
+    else if(mes==8){mess="Agosto";}
+    else if(mes==9){mess="Septiembre";}
+	else if(mes==10){mess="Octubre";}
+    else if(mes==11){mess="Noviembre";} 
+    else if(mes==12){mess="Diciembre";}
+	cout<<"Su fecha es: "<<dia<<" / "<<mess<<" / "<<years; cout<<"\n\n";
+	system("pause");
+	return 0;
 }
+
